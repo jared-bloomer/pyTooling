c597a1b6d29dbd0e93ac504a06a7e62fdfe502af
jlbloomer <jared.bloomer@coxautoinc.com>
2020-03-30 10:19:23 -0400

    added getContactInfo()

--- generate_script_headers.py
+++ generate_script_headers.py
@@ -15,6 +15,7 @@ __license_url__ = "HTML Link to License"
 __version__ = "1.0.1"
 __maintainer__ = "firstName lastName"
 __email__ = "someone@someplace.org"
+__website__ = "http://www.mywebsite.com"
 __status__ = "Production/Development"
 __deprecated__ = "True/False"
 """
@@ -22,14 +23,11 @@ __deprecated__ = "True/False"
 """
 TODO:
     
-    Get Authors
-    Get Additional Credits
     Get Copyright Info
     Get License Type
     Get License URL
     Get Contact Info
         Contact
-        Name
         Email
         Website
     Get Status of project
@@ -81,3 +79,42 @@ def getAuthors():
     else:
         return (pauthor, aauthors, credits)
 
+def getContactInfo():
+    """
+        Get Contact Info
+            Contact
+            Email
+            Website
+    """
+    try:
+        userName = input("What is your Name? ")
+        if not userName:
+            raise ValueError("Contact Name is required!")
+    except ValueError as e:
+        return Exception(f"Failed to get Contact Name. Failure was {e}")
+
+    try:
+        userEmail = input("What is your Email Address? ")
+        if not userEmail:
+            raise ValueError("Email Address is required!")
+    except ValueError as e:
+        return Exception(f"Failed to get Email Address. Failure was {e}")
+
+    try:
+        userWebsite = input("What is your Website? ")
+        if not userWebsite:
+            userWebsite = ""
+    except ValueError as e:
+        return Exception(f"Failed to get Website. Failure was {e}")
+
+    return (userName, userEmail, userWebsite)
+
+def main():
+    date = getTodaysDate()
+    (pauthor, aauthor, credits) = getAuthors()
+    (userName, userEmail, userWebste) = getContactInfo()
+
+##############
+# MAIN LOGIC #
+##############
+#main()
